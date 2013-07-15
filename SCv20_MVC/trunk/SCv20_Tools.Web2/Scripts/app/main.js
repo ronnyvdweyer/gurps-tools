86bf565b082ec326b6fd388dfef63fffea757018
andrade.dalton <andrade.dalton@5ee06d36-1d9e-6fd9-31ee-6c710a17e791>
2013-07-15 11:48:48 +0000

    Added SPA Dependencies

---
+++ SCv20_MVC/trunk/SCv20_Tools.Web2/Scripts/app/main.js
@@ -0,0 +1,34 @@
+﻿//http://kroltech.com/2013/03/building-a-web-app-using-backbone-js-and-require-js-part-1/
+
+(function () {
+    var root = this;
+    require.config({
+        baseUrl: "/scripts/app/",
+        paths: {
+            "underscore" : "../underscore.min",
+            "backbone"   : "../backbone.min",
+            "moment"     : "../moment.min"
+          //"jquery"     : "../jquery-1.9.1.min"   
+        },
+        shim: {
+            "underscore": {
+                exports: '_'
+            },
+            "backbone"   : {
+                deps     : ['underscore'/*, 'jquery'*/],
+                exports  : 'Backbone'
+            },
+            'jquery.masonry'      : { deps: ['jquery'], exports: 'masonry' },
+            'jquery.imagesLoaded' : { deps: ['jquery'], exports: 'imagesloaded' },
+            'jquery.simplemodal'  : { deps: ['jquery'], exports: 'modal' }
+        }
+    });
+
+    //these are defined externally so they can be used in our view templates:
+    define('jquery', [], function () { return root.jQuery; });
+    define('moment', [], function () { return root.moment; });
+
+    //require(['app'], function (app) {
+    //    app.run();
+    //});
+})();
